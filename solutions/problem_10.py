887f4373ab80a7614c3188a885240ffa499f4a3e
spicyNoodles15 <pressley.rodney@gmail.com>
2022-03-02 22:05:45 -0500

    add file back to repo

--- solutions/problem_10.py
+++ solutions/problem_10.py
@@ -1,6 +1,6 @@
 """
-###PROBLEM 10###
-# https://projecteuler.net/problem=10 #
+PROBLEM 10
+https://projecteuler.net/problem=10
 Summation of primes
 
 The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
