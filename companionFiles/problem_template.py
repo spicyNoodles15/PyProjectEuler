ceecb51c3cda09722382810fba8fd6c53f826089
spicyNoodles15 <pressley.rodney@gmail.com>
2022-03-31 21:58:31 -0400

    added problem template

---
+++ companionFiles/problem_template.py
@@ -0,0 +1,36 @@
+"""
+PROBLEM xx
+https://projecteuler.net/problem=xx
+
+"""
+# Libraries
+import time
+
+# Variables #
+answer = 0
+
+
+def solution():
+    '''
+    Solution xx
+    '''
+
+    # Track Execution time
+    start = time.time()
+
+    # FUNCTION CALLS
+
+    # Print Execution Time
+    end = time.time()
+    print(f"Execution time: {(end-start):.3f}s")
+
+    return answer
+
+def sol_text():
+    '''
+    Text of problem xx
+    '''
+    return '''Title:
+
+Text description
+'''
