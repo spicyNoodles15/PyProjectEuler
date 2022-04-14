ee9554383d5320f171d17c3abf4002ea55cac047
spicyNoodles15 <pressley.rodney@gmail.com>
2022-04-13 20:53:56 -0400

    solved problem 9

---
+++ solutions/problem_9.py
@@ -0,0 +1,47 @@
+"""
+PROBLEM 9
+https://projecteuler.net/problem=9
+
+"""
+# Libraries
+import time
+from math import sqrt
+
+# Variables #
+RANGE = 1000
+
+def solution():
+    '''
+    Solution 9
+    '''
+
+    # Track Execution time
+    start = time.time()
+
+    for a in range(1, RANGE):
+        for b in range(a+1, RANGE):
+            c = sqrt((a*a)+(b*b))
+            if c > b:
+                if (a+b+c == RANGE) and c.is_integer():
+                    # finally determine the product
+                    print(f'Answer: {(a*b*c)}, a={a},b={b},c={c}')
+                    return (a*b*c)
+
+    # Print Execution Time
+    end = time.time()
+    print(f"Execution time: {(end-start):.3f}s")
+
+    return 1
+
+def sol_text():
+    '''
+    Text of problem 9
+    '''
+    return '''Special Pythagorean triplet:
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which
+a^2+b^2=c^2
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+'''
